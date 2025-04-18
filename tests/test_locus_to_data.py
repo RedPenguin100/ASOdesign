a53951eadd170fc2b9b969211790f135d645fe8b
Michael Kovaliov <kovaliov@mail.tau.ac.il>
2025-04-19 00:48:10 +0300

    Added fast cache to common human genes that we deal with in the ASOptimizer data

--- tests/test_locus_to_data.py
+++ tests/test_locus_to_data.py
@@ -1,7 +1,10 @@
-from asodesigner.read_yeast import get_locus_to_data_dict_alternative, get_locus_to_data_dict
+import read_human_genome
 import pytest
+import pickle
 
+from asodesigner.consts import CACHE_DIR
 from asodesigner.timer import Timer
+from asodesigner.read_yeast import get_locus_to_data_dict_alternative, get_locus_to_data_dict
 
 
 @pytest.mark.slow
@@ -28,8 +31,35 @@ def test_locus():
 
 @pytest.mark.slow
 def test_intron_regression():
+    intron_yeast_gene = 'YNCA0002W'
+
     locus_to_data = get_locus_to_data_dict_alternative()
 
-    assert len(locus_to_data['YNCA0002W'].introns) == 1
-    intron = locus_to_data['YNCA0002W'].introns[0]
+    assert len(locus_to_data[intron_yeast_gene].introns) == 1
+    intron = locus_to_data[intron_yeast_gene].introns[0]
     assert str(intron) == 'CGACTTCCTGATTAAACAGGAAGACAAAGCA'
+
+
+@pytest.mark.slow
+def test_begin_regression_human():
+    locus_to_data = read_human_genome.get_locus_to_data_dict(gene_subset=['KLKB1'])
+
+    first_exon_begin = locus_to_data['KLKB1'].exons[0][0:33]
+
+    assert str(first_exon_begin) == 'AGTGCCACATTAGAACAGCTTGAAGACCGTTCA'
+
+    full_mrna_begin = locus_to_data['KLKB1'].full_mrna
+    idx = full_mrna_begin.find(first_exon_begin)
+    assert idx == 18528
+
+# not slow!
+def test_begin_regression_fast_human():
+    cache_path = CACHE_DIR / 'gene_to_data_simple_cache.pickle'
+    with open(cache_path, 'rb') as f:
+        gene_to_data = pickle.load(f)
+
+    first_exon_begin = gene_to_data['KLKB1'].exons[0][0:33]
+    assert str(first_exon_begin) == 'AGTGCCACATTAGAACAGCTTGAAGACCGTTCA'
+    full_mrna_begin = gene_to_data['KLKB1'].full_mrna
+    idx = full_mrna_begin.find(first_exon_begin)
+    assert idx == 18528
