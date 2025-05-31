2ae9bbae9c55ea7ed7d87d72a34aaa2882f8b949
Michael Kovaliov <kovaliov@mail.tau.ac.il>
2025-05-31 04:02:23 +0300

    Complete CPU and GPU simulations, modularity still poor

--- molecular_dynamics/run_md.py
+++ molecular_dynamics/run_md.py
@@ -3,6 +3,7 @@ import os
 import re
 import subprocess
 import sys
+import time
 
 import pexpect
 
@@ -34,9 +35,10 @@ def compile_nab(shell, nab_path):
     shell.sendline("./a.out")
     shell.expect(r'\$ ')
 
-def check_success(shell):
+def check_success(shell, timeout=10):
+    # convoluted STATUS message to filter accurately
     shell.sendline('echo __STATUS__$?__')
-    shell.expect(r'__STATUS__(\d+)__')
+    shell.expect(r'__STATUS__(\d+)__', timeout=timeout)
 
     success_status = int(shell.match.group(1)) # Match the number in the regex
     return success_status
@@ -70,29 +72,87 @@ def setup_shell():
 
 
 def run_tleap(shell):
-    shell.sendline('/home/michael/anaconda3/envs/AmberTools/bin/tleap -s -f usr_leap.in')
+    shell.sendline('/home/michael/anaconda3/envs/AmberTools/bin/tleap -s -f demo/usr_leap.in')
     shell.expect('\$ ')  # Wait for the new prompt to appear
     success_status = check_success(shell)
     if success_status != 0:
         raise ValueError("Error")
 
+def run_minimization1(shell,driver):
+    shell.sendline(f'{driver} -O -i demo/polyAT_wat_min1.in'
+                   ' -o polyAT_wat_min1.out -p polyAT_wat.prmtop -c polyAT_wat.rst7 -r polyAT_wat_min1.ncrst -ref polyAT_wat.rst7')
+    timeout = 600
+    shell.expect('\$ ', timeout=timeout)
+
+    success_status = check_success(shell, timeout=timeout) # minimization is slow, increase timeout for expect
+    if success_status != 0:
+        raise ValueError("Error")
+
+
+def run_minimization2(shell, driver):
+    shell.sendline(f'{driver} -O -i demo/polyAT_wat_min2.in -o polyAT_wat_min2.out'
+                   ' -p polyAT_wat.prmtop -c polyAT_wat_min1.ncrst -r polyAT_wat_min2.ncrst')
+    timeout = 600
+    shell.expect('\$ ', timeout=timeout)
+
+    success_status = check_success(shell, timeout=timeout) # minimization is slow, increase timeout for expect
+    if success_status != 0:
+        raise ValueError(f"Error {success_status}")
+
+def run_minimization3(shell, driver):
+    shell.sendline(f'{driver} -O -i demo/polyAT_wat_md1.in -o polyAT_wat_md1.out -p polyAT_wat.prmtop'
+                   ' -c polyAT_wat_min2.ncrst -r polyAT_wat_md1.ncrst -x polyAT_wat_md1.nc -ref polyAT_wat_min2.ncrst')
+    timeout = 600
+    shell.expect('\$ ', timeout=timeout)
+
+    success_status = check_success(shell, timeout=timeout) # minimization is slow, increase timeout for expect
+    if success_status != 0:
+        raise ValueError(f"Error {success_status}")
+
+def run_minimization4(shell, driver):
+    shell.sendline(f'{driver} -O -i demo/polyAT_wat_md2.in -o polyAT_wat_md2.out '
+                   '-p polyAT_wat.prmtop -c polyAT_wat_md1.ncrst -r polyAT_wat_md2.ncrst -x polyAT_wat_md2.nc')
+    timeout = 3600
+    shell.expect('\$ ', timeout=timeout)
+    success_status = check_success(shell, timeout=timeout)
+    if success_status != 0:
+        raise ValueError(f'Error {success_status}')
+
+
 def main():
+    cpu_driver = 'home/michael/anaconda3/envs/AmberTools/bin/sander'
+    gpu_driver = '/home/michael/molecular_dynamics/install/pmemd24_src/build/src/pmemd/src/pmemd.cuda_DPFP'
+
+    driver = gpu_driver
+
+
     shell = setup_shell()
+    shell.logfile = sys.stdout
 
     AMBER_CLASSIC_PATH = '/home/michael/molecular_dynamics/AmberClassic'
 
-    shell.logfile = sys.stdout
-
-    #######################################
-    ##### Setup conda environment
     setup_conda_env(shell, conda_env_name='AmberTools')
-
-    #######################################
-    #### AmberClassic
     setup_amber_classic(shell, AMBER_CLASSIC_PATH)
 
-    compile_nab(shell, 'demo_nuc.nab')
+
+    compile_nab(shell, 'demo/demo_nuc.nab')
     run_tleap(shell)
+    start = time.time()
+    run_minimization1(shell, driver)
+    end = time.time()
+    print(f"Min1 took: {end - start}")
+    start = time.time()
+    run_minimization2(shell, driver)
+    end = time.time()
+    print(f"Min2 took: {end - start}")
+    start = time.time()
+    run_minimization3(shell, driver)
+    end = time.time()
+    print(f"Min3 took: {end - start}")
+    start = time.time()
+    run_minimization4(shell, driver)
+    end = time.time()
+    print(f"Min4 took: {end - start}")
 
 
 if __name__ == '__main__':
