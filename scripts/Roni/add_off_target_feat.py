aec6946d6ec965ecb5e405b396a68db029ff33e2
RoniZarakhovsky <ronioof23@gmail.com>
2025-07-01 09:50:32 +0300

    code that creates a vector of indexes (from May's data) and the off-target score

---
+++ scripts/Roni/add_off_target_feat.py
@@ -0,0 +1,129 @@
+from asodesigner.fold import get_mfe_scores, get_trigger_mfe_scores_by_risearch, Interaction, dump_target_file
+
+import pandas as pd
+from io import StringIO
+import ViennaRNA as RNA
+import numpy as np
+
+import os
+import re
+import random
+import subprocess
+from Bio.Seq import Seq
+from typing import Dict, List
+from enum import Enum
+
+from asodesigner.consts import RISEARCH1_BINARY_PATH, TMP_PATH ,RISEARCH1_PATH
+
+
+# Set base directory relative to script
+script_dir = os.path.dirname(__file__)
+data_dir = os.path.abspath(os.path.join(script_dir, "..", "data_genertion"))
+
+# Load the main ASO dataset
+data_path = os.path.join(data_dir, "data_asoptimizer_updated.csv")
+may_df = pd.read_csv(data_path)
+may_df = may_df.head(4)
+
+# Expression files path
+expr_path = os.path.join(data_dir, "cell_line_expression")
+
+# Load each transcriptome file
+A431_df = pd.read_csv(os.path.join(expr_path, 'ACH-001328_transcriptome_top500.csv'))
+NCI_H460_df = pd.read_csv(os.path.join(expr_path, 'ACH-000463_transcriptome_top500.csv'))
+SH_SY5Y_df = pd.read_csv(os.path.join(expr_path, 'ACH-001188_transcriptome_top500.csv'))
+HeLa_df = pd.read_csv(os.path.join(expr_path, 'ACH-001086_transcriptome_top500.csv'))
+HepG2_df = pd.read_csv(os.path.join(expr_path, 'ACH-000739_transcriptome_top500.csv'))
+U_251MG_df = pd.read_csv(os.path.join(expr_path, 'ACH-000232_transcriptome_top500.csv'))
+
+cell_line_list = ['ACH-001328', 'ACH-000463', 'ACH-001188', 'ACH-001086', 'ACH-000739', 'ACH-000232']
+
+cell_line2df_ = {'A431':A431_df,
+                'A-431':A431_df,
+                'NCI-H460':NCI_H460_df,
+                'SH_SY5Y':SH_SY5Y_df,
+                'HeLa':HeLa_df,
+                'HepG2':HepG2_df,
+                'U-251MG':U_251MG_df}
+
+
+def parse_risearch_output(output_str: str) -> pd.DataFrame:
+    columns = ["trigger", "trigger_start", "trigger_end", "target", "target_start", "target_end", "score", "energy"]
+    df = pd.read_csv(StringIO(output_str.strip()), sep="\t", header=None, names=columns)
+    return df
+
+def aggregate_off_targets(df: pd.DataFrame) -> pd.DataFrame:
+    # Aggregate: sum score (if it's hybridization hits) and take minimum (strongest) energy
+    grouped = df.groupby("target").agg({
+        "score": "sum",
+        "energy": "min"
+    }).reset_index()
+    return grouped
+
+
+def get_off_target_feature(cell_line2df, ASO_df):
+    index_score_vec = {}
+
+    for idx, row in ASO_df.iterrows():
+
+        index = row['index']
+        cell_line = row['Cell_line']
+        ASO_seq = row['Sequence']
+        target_gene = row['Canonical Gene Name']
+
+        if cell_line not in cell_line2df:
+            continue
+
+        curr_df = cell_line2df[cell_line]
+
+        name_to_seq = {}
+        name_to_exp = {}
+
+        for _, gene_row in curr_df.iterrows():
+            curr_gene = gene_row['Gene'].split()[0]
+
+            # Skip the target gene
+            if curr_gene == target_gene:
+                continue
+
+            # Select mutated or original sequence
+            mut_seq = gene_row.get('Mutated Transcript sequence')
+            og_seq = gene_row.get('Original Transcript sequence')
+
+            if pd.isna(mut_seq) and pd.isna(og_seq):
+                continue
+
+            mRNA_seq = mut_seq if not pd.isna(mut_seq) else og_seq
+            exp_level = gene_row['expression_TPM']
+
+            name_to_seq[curr_gene] = mRNA_seq
+            name_to_exp[curr_gene] = exp_level
+
+
+        # Calculate mfe scores
+        result_dict = get_trigger_mfe_scores_by_risearch(
+            ASO_seq,
+            name_to_seq,
+            minimum_score=800,
+            parsing_type='2'
+        )
+        result_df = parse_risearch_output(result_dict)
+        print(f'001: {result_df}')
+        result_df_agg = aggregate_off_targets(result_df)
+        print(f'002: {result_df_agg}')
+
+        # Weight by expression
+        result_dict_weighted = {
+            row['target']: row['energy'] * name_to_exp.get(row['target'], 0)
+            for _, row in result_df_agg.iterrows()
+        }
+        print(result_dict_weighted)
+        index_score_vec[index] = sum(result_dict_weighted.values())
+
+
+    return index_score_vec
+
+
+off_target_vec = get_off_target_feature(cell_line2df_, may_df)
+print(off_target_vec)
+
