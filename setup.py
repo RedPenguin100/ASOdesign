51e01bb6a1ed9d1ad1a315c82b6ce465a8631174
Din-Saadon <din.saadon1@gmail.com>
2025-09-30 11:26:04 +0300

    Slim production branch: gather only the important functions for deployment

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ setup(
     version="0.1.0",
     author="TAU_iGEM_2025",
     author_email="michaelkovaliov97@gmail.com",
-    description="A short description of my_package",
+    description="A short description of asodesigner",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/RedPenguin100/ASOdesign",
-    packages=find_packages(where="asodesigner"),
-    package_dir={"": "asodesigner"},
+    packages=find_packages(),  # <- automatically finds asodesigner/
     install_requires=[
         "numpy",
         "viennarna",
         "pandas",
-
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
